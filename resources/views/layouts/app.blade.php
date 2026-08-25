281a2c5bd26cb09108eaf0bc107aa4c0a8bbde7d
ThoriqMFatih <thoriqmf3009@gmail.com>
2026-08-25 15:08:52 +0700

    Set website favicon from navbar logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,10 @@
 
     <title>{{ config('app.name', 'Vlora Nusantara') }}</title>
 
+    {{-- Website Favicon / Icon --}}
+    <link rel="icon" type="image/png" href="{{ asset('dokumentasi/photo_2026-01-28_10-23-22 - Edited.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('dokumentasi/photo_2026-01-28_10-23-22 - Edited.png') }}">
+
     {{-- Bootstrap 5 CSS (CDN) --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
